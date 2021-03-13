0488783dff461d1dbbae3f08f8d91359c8ffdb4b
Miguel Rios <miguelriosoliveira@gmail.com>
2021-03-12 22:50:12 -0300

    Serving static HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
 	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
 	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+	],
 	module: {
 		rules: [
 			{
