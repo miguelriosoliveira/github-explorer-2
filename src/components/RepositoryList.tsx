9b11c0bd2a11f1b8802b724324238db3f7dd28ca
Miguel Rios <miguelriosoliveira@gmail.com>
2021-03-14 21:17:10 -0300

    TypeScript components

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,14 +4,14 @@ import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
-// const repository = {
-// 	name: 'unform2',
-// 	description: 'Forms in React',
-// 	link: 'https://github.com/unform/unform',
-// };
+interface Repository {
+	name: string;
+	description: string;
+	html_url: string;
+}
 
 export function RepositoryList() {
-	const [repositories, setRepositories] = useState([]);
+	const [repositories, setRepositories] = useState<Repository[]>([]);
 
 	useEffect(() => {
 		fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -24,8 +24,8 @@ export function RepositoryList() {
 			<h1>Lista de repositórios</h1>
 
 			<ul>
-				{repositories.map(repo => (
-					<RepositoryItem key={repo.name} repository={repo} />
+				{repositories.map(repository => (
+					<RepositoryItem key={repository.name} repository={repository} />
 				))}
 			</ul>
 		</section>
